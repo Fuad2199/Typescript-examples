dd795e9c75836fb3fdb1a77c1f6068c212b2e4bd
Fuad2199 <fuad.beybutov099@gmail.com>
2025-04-16 14:00:00 +0400

    added interfaces

--- typescript/src/alias-interfaces.ts
+++ typescript/src/alias-interfaces.ts
@@ -19,3 +19,32 @@ const car: Car = {
 }
 
 console.log(car)
+
+// Interfaces are similar to type aliases, except they only apply to object types.
+interface Rectangle {
+    height: number;
+    width: number
+};
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
+};
+console.log(rectangle)
+
+// Extending an interface means you are creating a new interface with the same properties as the original, plus something new.
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+interface ColoredRectangle extends Rectangle {
+    color: string
+}
+
+const coloredRectangle: ColoredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red"
+};
+console.log(coloredRectangle)
