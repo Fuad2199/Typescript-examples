a5a1cffcebaa994b2f4aa26043d6d1a8a5d62896
Fuad2199 <fuad.beybutov099@gmail.com>
2025-05-04 23:54:07 +0400

    union types updated

--- typescript/src/union-types.ts
+++ typescript/src/union-types.ts
@@ -4,10 +4,17 @@
 // Union | (OR)
 // Using the | we are saying our parameter is a string or number:
 
-function printStatusCode(code: string | number) {
-    console.log(`My status code is ${code}.`)
-  }
+// function printStatusCode(code: string | number) {
+//     console.log(`My status code is ${code}.`)
+//   }
   
-  printStatusCode(404);
-  printStatusCode('404');
+//   printStatusCode(404);
+//   printStatusCode('404');
 
+//   Union Type Errors
+//   Note: you need to know what your type is when union types are being used to avoid type errors:
+// function printStatusCode(code: string | number) {
+    // console.log(`My status code is ${code.toUppercase()}.`)  // error: Property
+// }
+// 'toUpperCase' does not exist on type 'string | number'. Property 'toUpperCase' does not exist 
+// on type 'number'
